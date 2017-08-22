0642bf38eace4c0b23e9e7230d6513f771489882
Haakon Løtveit <haakon.lotveit@visma.com>
2017-08-22 15:57:27 +0200

    Classes that represent Norwegian phone numbers accurately. Not all types are represented.

---
+++ src/main/java/no/smalltypes/telephone/norway/NorwegianPhoneNumber.java
@@ -0,0 +1,34 @@
+package no.smalltypes.telephone.norway;
+
+import no.smalltypes.telephone.AbstractTelephoneNumber;
+import no.smalltypes.telephone.IllegalPhoneNumberException;
+
+public abstract class NorwegianPhoneNumber extends AbstractTelephoneNumber {
+
+	public static final String COUNTRY = "NORWAY";
+
+	public NorwegianPhoneNumber(String terseNumber, String localPrettyPrintedNumber,
+			String internationalPrettyPrintedNumber) {
+		super(terseNumber, localPrettyPrintedNumber, internationalPrettyPrintedNumber, COUNTRY);
+	}
+
+	protected static String possiblyNorwegianNumberOrThrow(final String maybeNumber) {
+		// If the number starts with the international prefix, chop it off, and continue:
+		String normalizedNumber = maybeNumber.startsWith("+47")? maybeNumber.substring(3) : maybeNumber;
+
+		// If this is all digits, we're happy.
+		try {
+			onlyAsciiDigitsOrThrow(normalizedNumber);
+		}
+		catch(IllegalPhoneNumberException ipne) {
+			String errorMessage = String.format("After checking for international prefix for Norwegian phone numbers, the number '%s' had been reduced to '%s', which contained non-digit characters", maybeNumber, normalizedNumber);
+			throw new IllegalPhoneNumberException(errorMessage);
+		}
+
+		if(NorwegianPhonenumberType.classify(normalizedNumber) == NorwegianPhonenumberType.NotAPhoneNumber) {
+			throw new IllegalPhoneNumberException(String.format("Phone number '%s' is not a valid Norwegian phone number of any sort", maybeNumber));
+		}
+		
+		return normalizedNumber;
+	}
+}
---
+++ src/main/java/no/smalltypes/telephone/norway/NorwegianPhonenumberType.java
@@ -0,0 +1,81 @@
+package no.smalltypes.telephone.norway;
+
+import static java.lang.Long.parseLong;
+
+public enum NorwegianPhonenumberType {
+	Landline, Machine2Machine, Machine2MachineCellular, Cellphone, GlobalTitle, EmergencyNumber, GreaterGood, NonGeographicTollFree, NonGeographic5Digit, OperatorSpecific, DirectoryService, NotAPhoneNumber;
+
+	/**
+	 * This attempts to classify a given phone number as a Norwegian phone number.
+	 * This makes creating classes that specialize NorwegianPhoneNumber easier, by removing some of the validation burden, and allowing better error messaging.
+	 * @param terseRepresentation A terse representation of a number, and with the exception of the optional international prefix ("+47"), only digits.
+	 * @return the enumerated type that best fits the input. If no "real type" can be found, it will return NotAPhoneNumber, which is perfectly valid.
+	 */
+	public static NorwegianPhonenumberType classify(String terseRepresentation) {
+		String normalizedNumber = terseRepresentation.startsWith("+47")? terseRepresentation.substring(3) : terseRepresentation;		
+		long numerical = parseLong(normalizedNumber, 10);
+
+		// This is a normal landline phone number
+		if(normalizedNumber.length() == 8 &&
+				!normalizedNumber.startsWith("4") &&
+				!normalizedNumber.startsWith("58") &&
+				!normalizedNumber.startsWith("59") &&
+				!normalizedNumber.startsWith("8")) {
+			return Landline;
+		}
+
+		// 58 00 00 00 00 00 to 58 99 99 99 99 99 are reserved for machine to machine communication. This would give 10^10 addresses, and hence more than an uint32.
+		// That's right, in Norway, every ipv4 address could have been hooked to a phone number and you'd still have more than half of them unassigned.
+		// Which is kind of cool. You could also allow 100 machines on every normal phone number and still have a lot of free numbers.
+		// This is something I wish I knew more about. Sounds like a cool seventies futuretech thingie.
+		// Anyway, this is something that I wish I knew more about.
+		if(normalizedNumber.length() == 12 && normalizedNumber.startsWith("58")) {
+			return Machine2Machine;
+		}
+
+		if(normalizedNumber.length() == 8 && normalizedNumber.startsWith("59")) {
+			return Machine2MachineCellular;
+		}
+		if(normalizedNumber.length() == 8 && (normalizedNumber.startsWith("4") || normalizedNumber.startsWith("9")) && numerical < 999_00_000) {
+			return Cellphone;
+		}
+
+		if(normalizedNumber.length() == 8 && numerical >= 999_00_000) {
+			return GlobalTitle;
+		}
+
+		// This means that it's an emergency service. 110, 112 and 113 are the standards, but all these numbers are reserved, and hence possible numbers.
+		// Or rather, 100 - 189 inclusive is for emergency numbers and 190-199 are operator specific.
+		if(normalizedNumber.length() == 3 && numerical >= 100 && numerical < 190) {
+			return EmergencyNumber;
+		}
+
+		// This is for harmonized services of social value. Yes, that's a EU bureaucrat term. How did you know?
+		if(normalizedNumber.length() == 6 && normalizedNumber.startsWith("116")) {
+			return GreaterGood;
+		}
+
+		// Non Geographic 800-number. TODO:
+		if(normalizedNumber.length() == 8 && normalizedNumber.startsWith("8")) {
+			return NonGeographicTollFree;
+		}
+
+		// This is the type of numbers beholden to non-geographically attached landlines. Usually taxi services. My hometown has 07000 for example.
+		if(normalizedNumber.length() == 5 && normalizedNumber.startsWith("0") && normalizedNumber.charAt(1) != '1') {
+			return NonGeographic5Digit;
+		}
+
+		if(normalizedNumber.length() == 3 && numerical >= 190 && numerical <= 199) {
+			return OperatorSpecific;
+		}
+
+		// This is for directory services, like 1880 and 1881.
+		if(normalizedNumber.length() == 4 && numerical >= 1850 && numerical <= 1899) {
+			return DirectoryService;
+		}
+
+		return NotAPhoneNumber;
+
+	}
+}
+
